17a19dc211a910f6762c51136941a0380e56f2ae
Alexey Osipov <114313704+DarVar1@users.noreply.github.com>
2023-01-04 17:10:51 +0300

    Add files via upload

--- 13.8.19.py
+++ 13.8.19.py
@@ -1,20 +1,21 @@
 def amount():
     if person_age<=25 and tickets_count>=3:
-        num = tickets_discount*990*tickets_count
+        num = tickets_discount*990
     elif person_age>25 and tickets_count>=3:
-        num = tickets_discount*1390*tickets_count
+        num = tickets_discount*1390
     elif person_age<=25 and tickets_count<3:
-        num = 990*tickets_count
+        num = 990
     elif person_age>25 and tickets_count<3:
-        num = 1390*tickets_count
+        num = 1390
     return num
     
 tickets_count = int(input("Введите кол-во билетов: "))
 tickets_discount = 0.9
-person_age = int(input("Ваш возраст: "))
+person_age = 0
 final_amount = 0
-if person_age>18:
-    final_amount = amount()
-    print("Сумма к оплате:",final_amount)
-else:
-    print("Сумма к оплате:",final_amount)
+for i in range(tickets_count):
+    print("Билет №:",i+1)
+    person_age = int(input("Ваш возраст: "))
+    if person_age>18:
+        final_amount += amount()
+print("Сумма к оплате:",final_amount)
